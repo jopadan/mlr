7b42cdf3a16f9793c386823049064679181b1a31
Jon Daniel <joneqdaniel@gmail.com>
2025-02-11 18:14:55 +0100

    update example

--- examples/example.cpp
+++ examples/example.cpp
@@ -4,25 +4,21 @@ using namespace math;
 
 bool test_cross()
 {
-	vec::f64<4> src[4] = { vec::f64<4>::identity(0),
-	                       vec::f64<4>::identity(1),
-			       vec::f64<4>::load3(vec::f64<3, align::vector>::cross3(src[0],src[1]).data(),0),
-			       vec::f64<2>::cross4(src[0],src[1],src[2]) };
-
-	src[0].print_header(4, "vector");
-	src[0].print(4);
-	src[1].print(4);
-	src[2].print_header(4, "cross3");
-	src[2].print(4);
-	src[3].print_header(4, "cross4");
-	src[3].print(4);
-	std::cout << std::endl;
-	src[0].print_header(2, "vector");
-	src[0].print(2);
-	vec::f64<2>   a[2] = { vec::f64<2>::cross2(src[0], GL_CCW), vec::f64<2>::cross2(src[0], GL_CW) };
-	vec::f64<2>::print_header(2,"cross2 GL_CCW/GL_CW");
-	a[0].print(2);
-	a[1].print(2);
+	vec::f32<4> v1[4] = { vec::f32<4>::identity(0),
+	                      vec::f32<4>::identity(1),
+			      vec::f32<4>::load3(vec::f32<3, align::vector>::cross3(v1[0],v1[1]).data(),0),
+			      vec::f32<2>::cross4(v1[0],v1[1],v1[2]) };
+	vec::f32<2> v2[2] = { vec::f32<2>::cross2(v1[0], GL_CCW), vec::f32<2>::cross2(v1[0], GL_CW) };
+	v1[0].print_header(4, "vector");
+	v1[0].print(4);
+	v1[1].print(4);
+	v1[2].print_header(4, "cross3");
+	v1[2].print(4);
+	v1[3].print_header(4, "cross4");
+	v1[3].print(4);
+	vec::f32<4> v3 = v2[0];
+	v3.print_header(4, "cross2 GL_CCW/GL_CW");
+	v3.print(4);
 	std::cout << std::endl;
 	return true;
 }
